03f1f33007110562cd0dd6c80eb372ea4fa68c68
shoaib Hussain <107783902+Shoaib7457@users.noreply.github.com>
2024-10-02 15:11:46 +0530

    Update App.js

--- maps/src/App.js
+++ maps/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch('/api/vehicle/current') // Fetch from backend
+      fetch('http://localhost:5000/api/vehicle/current') // Fetch from backend
         .then((response) => response.json())
         .then((data) => {
           const newPosition = [data.latitude, data.longitude];
