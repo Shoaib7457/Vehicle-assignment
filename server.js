e19451f5402061b607805433702e1368d8a1e9bc
shoaib Hussain <107783902+Shoaib7457@users.noreply.github.com>
2024-10-02 19:27:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.get('/api/vehicle/current', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
