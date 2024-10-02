d8855930448f8d2751f44dffa5c24993861c9bdc
shoaib Hussain <107783902+Shoaib7457@users.noreply.github.com>
2024-10-02 15:36:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 // // Serve static files from the React app
@@ -59,6 +59,6 @@ app.get('/api/vehicle/current', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
